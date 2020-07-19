1190dd9becd6255c997239b9ecc9378334a3559a
Mark Lopresto <marklopresto@Marks-iMac.local>
2020-07-19 15:52:14 -0400

    move to react markdown

--- A02 - Markdown Editor/start/src/App.js
+++ A02 - Markdown Editor/start/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import ReactMarkdown from "react-markdown";
+import './App.css'
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# sup')
+
+  // onChange set the value of the text area
+  function handleChange(event) {
+    setMarkdown(event.target.value)
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={handleChange} value={markdown} />
 
-      <div className="preview" />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
