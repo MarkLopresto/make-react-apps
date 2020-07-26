ff7a8acc02b30d0d2d367ac6b80970c4b339f609
Mark Lopresto <marklopresto@Marks-iMac.local>
2020-07-26 14:02:19 -0400

    refactor code into components…
    refactor code into components. Leave Router. import Navigation component. import Routes component.

--- A03 - Highlight Tabs/start/src/App.js
+++ A03 - Highlight Tabs/start/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import Navigation from './components/Navigation'
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./components/Routes";
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <div className="tab">
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <Navigation />
+          <div className="viewport">
+            <Routes />
           </div>
         </div>
-
-        <div className="viewport">Pages Go Here</div>
       </div>
-    </div>
+    </Router>
   );
 }
 
